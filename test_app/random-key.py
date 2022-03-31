433dd085725fb1394110f903723d58c98a1120ef
Justin Haydt <justin@haydt.io>
2022-03-30 23:01:18 -0700

    adjust error handling

--- test_app/random-key.py
+++ test_app/random-key.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from flask import Flask
+from webbrowser import get
+from flask import Flask, redirect, url_for
 from base64 import b64encode
 from waitress import serve
 import os
@@ -16,10 +17,18 @@ app = Flask(__name__)
 @app.route('/key/<int:bytes_number>', methods=['GET'])
 def get_hash(bytes_number):
     result = b64encode(os.urandom(bytes_number))
-    return result
+    return result + b'\n'
+
+# define healthcheck path used by k8s deployment
 @app.route('/healthz', methods=['GET'])
 def healthz():
     return "OK"
+
+# route for any path not found to redirect to get_hash function
+@app.errorhandler(404)
+def page_not_found(error):
+#    return redirect(url_for('get_hash', bytes_number=404))
+    return "HTTP 404: Try path /key/$integer \n", 404
 
 # Set port to listen on from arguments
 if __name__ == '__main__':
